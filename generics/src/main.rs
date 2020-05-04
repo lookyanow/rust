16d74e75b9c7e2341ccde66f5339ae6b7aeee63e
Ivan Lukyanow <i.lukyanov@city-mobil.ru>
2020-05-04 18:50:29 +0300

    generics project added

---
+++ generics/src/main.rs
@@ -0,0 +1,53 @@
+fn largest_i32(list: &[i32]) -> i32 {
+    let mut largest = list[0];
+
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+fn largest_char(list: &[char]) -> char {
+    let mut largest = list[0];
+
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
+
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
+
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest_i32(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q', 'z'];
+
+    let result = largest_char(&char_list);
+    println!("The largest char is {}", result);
+
+    let new_list = vec![33, 10, 40, 20, 66];
+    let result = largest(&new_list);
+    println!("{}", result);
+
+    let result = largest(&char_list);
+    println!("{}", result)
+}
