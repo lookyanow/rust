1da7aa8b06592e10f5883239a955289e501cf115
Ivan Lukyanow <i.lukyanov@city-mobil.ru>
2020-06-24 19:25:03 +0300

    new hello tcp listener project added

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,3 +1,25 @@
+use std::net::TcpListener;
+use std::net::TcpStream;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("localhost:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+        println!("Connection established");
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+
+    stream.read(&mut buffer).unwrap();
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
