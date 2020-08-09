6f2bd037efa9d130bb71314daab5a7aec8dae072
Ivan Lukyanow <i.lukyanov@city-mobil.ru>
2020-08-09 11:50:57 +0300

    theads handler added

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -3,14 +3,19 @@ use std::net::TcpStream;
 use std::io::prelude::*;
 use std::fs;
 
+use std::thread;
+use std::time::Duration;
+
 fn main() {
     let listener = TcpListener::bind("localhost:7878").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
-        println!("Connection established");
+        thread::spawn(|| {
+            handle_connection(stream);
+        });
+
     }
 }
 
@@ -20,17 +25,24 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
-    if buffer.starts_with(get) {
-        let contents = fs::read_to_string("hello.html").unwrap();
-
-        let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
-
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
+    let(status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
     } else {
-        // another request
-    }
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    
+    let response = format!("{}{}", status_line, contents);
+    
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+    
 
 
 }
