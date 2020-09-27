db814c298fe0183cc2278fd9175af05a63911285
Ivan Lukyanow <i.lukyanov@city-mobil.ru>
2020-09-27 17:28:04 +0300

    rocket http framework example added

---
+++ hello-rocket/src/main.rs
@@ -0,0 +1,12 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+
+#[get("/hello/<name>")]
+fn hello(name: String) -> String {
+    format!("Hello, {}!", name.as_str())
+}
+
+fn main() {
+    rocket::ignite().mount("/", routes![hello]).launch();
+}
